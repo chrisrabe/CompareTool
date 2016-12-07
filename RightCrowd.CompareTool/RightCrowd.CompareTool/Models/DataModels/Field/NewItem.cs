0131a19191fbe897e1472e54e9567fe1ec5b2abe
Chris Rabe <chris.rabe14@gmail.com>
2016-12-08 07:08:12 +1000

    Testing whether it can commit newly created files

---
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/DataModels/Field/NewItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightCrowd.CompareTool.Models.DataModels.Field
+{
+    class NewItem
+    {
+        // This is a github test object... please remove
+    }
+}
