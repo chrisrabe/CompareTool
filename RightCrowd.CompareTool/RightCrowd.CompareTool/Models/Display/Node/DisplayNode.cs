b1948a95c53fb74b1753d85280bb6f1a92f904b9
Chris Rabe <chris.rabe14@gmail.com>
2017-01-23 11:24:27 +1000

    implemented the data node

---
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Display/Node/DisplayNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using RightCrowd.CompareTool.Models.DataModels.DataNode;
+using RightCrowd.CompareTool.Models.DataModels.Database;
+using RightCrowd.CompareTool.HelperClasses;
+
+namespace RightCrowd.CompareTool.Models.Display.Node
+{
+    public class DisplayNode : ObservableObject, IDisplayNode
+    {
+        private ObservableCollection<IDataNode> _dataNodes;
+
+        public DisplayNode(IDatabase database)
+        {
+            _dataNodes = database.Data;
+        }
+
+        /// <summary>
+        /// Gets or sets the data nodes of this display node.
+        /// </summary>
+        public ObservableCollection<IDataNode> DataNodes
+        {
+            get
+            {
+                if (_dataNodes == null)
+                    _dataNodes = new ObservableCollection<IDataNode>();
+
+                return _dataNodes;
+            }
+            set
+            {
+                if(_dataNodes != value)
+                {
+                    _dataNodes = value;
+                    OnPropertyChanged("DataNodes");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the type of this display node.
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
---
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Display/Node/IDisplayNode.cs
@@ -0,0 +1,21 @@
+﻿using RightCrowd.CompareTool.Models.DataModels.DataNode;
+using System.Collections.ObjectModel;
+
+namespace RightCrowd.CompareTool.Models.Display.Node
+{
+    /// <summary>
+    /// Stores a collection of IDataNodes with similar types.
+    /// </summary>
+    public interface IDisplayNode
+    {
+        /// <summary>
+        /// Gets or sets the type of this display node
+        /// </summary>
+        string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data nodes of this display node
+        /// </summary>
+        ObservableCollection<IDataNode> DataNodes { get; set; }
+    }
+}
