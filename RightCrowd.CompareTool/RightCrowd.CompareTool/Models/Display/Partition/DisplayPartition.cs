2c579d49f2bf7e7497e6da30890daedcf4f33769
Chris Rabe <chris.rabe14@gmail.com>
2017-01-23 12:20:32 +1000

    implemented the display partition

---
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Display/Partition/DisplayPartition.cs
@@ -0,0 +1,70 @@
+﻿using RightCrowd.CompareTool.Models.Display.Data;
+using RightCrowd.CompareTool.HelperClasses;
+
+namespace RightCrowd.CompareTool.Models.Display.Partition
+{
+    public class DisplayPartition : ObservableObject, IDisplayPartitions
+    {
+        private string _directory;
+        private IDisplayData _differences;
+        private IDisplayData _similarities;
+
+        public DisplayPartition(string directory, IDisplayData differences, IDisplayData similarities)
+        {
+            Directory = directory;
+            Differences = differences;
+            Similarities = similarities;
+        }
+
+        public string Directory
+        {
+            get
+            {
+                return _directory;
+            }
+
+            set
+            {
+                if(_directory != value)
+                {
+                    _directory = value;
+                    OnPropertyChanged("Directory");
+                }
+            }
+        }
+
+        public IDisplayData Differences
+        {
+            get
+            {
+                return _differences;
+            }
+
+            set
+            {
+                if(_differences != value)
+                {
+                    _differences = value;
+                    OnPropertyChanged("Differences");
+                }
+            }
+        }
+
+        public IDisplayData Similarities
+        {
+            get
+            {
+                return _similarities;
+            }
+
+            set
+            {
+                if(_similarities != value)
+                {
+                    _similarities = value;
+                    OnPropertyChanged("Similarities");
+                }
+            }
+        }
+    }
+}
--- RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Display/Partition/IDisplayPartition.cs
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Display/Partition/IDisplayPartition.cs
@@ -1,11 +1,26 @@
-﻿namespace RightCrowd.CompareTool.Models.Display.Partition
+﻿using RightCrowd.CompareTool.Models.Display.Data;
+
+namespace RightCrowd.CompareTool.Models.Display.Partition
 {
     /// <summary>
     /// Stores the comparison result for one database. In the compare view,
     /// The data stored in this class is used in one of the panel partitions.
     /// </summary>
-    public interface IDisplayPartition
+    public interface IDisplayPartitions
     {
-        
+        /// <summary>
+        /// Gets or sets the directory which this partition is refering to.
+        /// </summary>
+        string Directory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the differences
+        /// </summary>
+        IDisplayData Differences { get; set; }
+
+        /// <summary>
+        /// Gets or sets the similarities
+        /// </summary>
+        IDisplayData Similarities { get; set; }
     }
 }
