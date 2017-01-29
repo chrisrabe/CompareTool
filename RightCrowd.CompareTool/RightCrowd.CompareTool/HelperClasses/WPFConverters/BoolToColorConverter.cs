5f2aea7d02372844acdbcda97ca3944aba989fc8
Chris Rabe <chris.rabe14@gmail.com>
2017-01-30 09:44:35 +1000

    added the base boolean to color converter

---
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/HelperClasses/WPFConverters/BoolToColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RightCrowd.CompareTool.HelperClasses.WPFConverters
+{
+    /// <summary>
+    /// Converts a boolean to the specified colors. If the boolean is false, it returns the normal color, 
+    /// if the boolean is true, it returns the error color.
+    /// </summary>
+    public abstract class BoolToColorConverter : IValueConverter
+    {
+        private SolidColorBrush _normalColor;
+        private SolidColorBrush _errorColor;
+
+        public BoolToColorConverter(SolidColorBrush normalColor, SolidColorBrush errorColor)
+        {
+            _normalColor = normalColor;
+            _errorColor = errorColor;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return _errorColor;
+            else
+                return _normalColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? value.Equals(_normalColor) : false;
+        }
+    }
+}
