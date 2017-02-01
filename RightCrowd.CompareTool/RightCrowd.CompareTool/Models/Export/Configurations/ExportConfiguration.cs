9dc4928d25c71f3951ee30b5ba5418456cf6a022
Chris Rabe <chris.rabe14@gmail.com>
2017-02-01 12:35:23 +1000

    Implemented saving to text files

--- RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Export/Configurations/ExportConfiguration.cs
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/Models/Export/Configurations/ExportConfiguration.cs
@@ -6,15 +6,15 @@ namespace RightCrowd.CompareTool.Models.Export.Configurations
     /// This class contains properties which determines the output when
     /// exporting the results of the comparison data.
     /// </summary>
-    public class ExportConfiguration : ObservableObject
+    public class ExportConfiguration
     {
-        #region Fields
 
-        private bool _includeSimilarFields;
-        private bool _db1Only;
-        private bool _db2Only;
-
-        #endregion // Fields
+        public ExportConfiguration(bool includeSimilarFields, bool db1Only, bool db2Only)
+        {
+            IncludeSimilarFields = includeSimilarFields;
+            DB1Only = db1Only;
+            DB2Only = db2Only;
+        }
 
         #region Properties
 
@@ -24,56 +24,18 @@ namespace RightCrowd.CompareTool.Models.Export.Configurations
         /// If false, it should only export the fields which are marked
         /// different.
         /// </summary>
-        public bool IncludeSimilarFields
-        {
-            get { return _includeSimilarFields; }
-            set
-            {
-                if (_includeSimilarFields != value)
-                {
-                    _includeSimilarFields = value;
-                    OnPropertyChanged("IncludeSimilarFields");
-                }
-            }
-        }
-
+        public bool IncludeSimilarFields { get; set; }
         /// <summary>
         /// If true, it should export only the comparison results for database
         /// one.
         /// </summary>
-        public bool DB1Only
-        {
-            get { return _db1Only; }
-            set
-            {
-                if (_db1Only != value)
-                {
-                    _db1Only = value;
-                    OnPropertyChanged("DB1Only");
-                    if (_db1Only)
-                        DB2Only = false; // only one flag can be set
-                }
-            }
-        }
+        public bool DB1Only { get; set; }
 
         /// <summary>
         /// If true, it should only export the comparison results for database
         /// two.
         /// </summary>
-        public bool DB2Only
-        {
-            get { return _db2Only; }
-            set
-            {
-                if (_db2Only != value)
-                {
-                    _db2Only = value;
-                    OnPropertyChanged("DB2Only");
-                    if (_db2Only)
-                        DB1Only = false; // only one flag can be set
-                }
-            }
-        }
+        public bool DB2Only { get; set; }
 
         #endregion // Properties
     }
--- RightCrowd.CompareTool/RightCrowd.CompareTool/ViewModels/ExportViewModel.cs
+++ RightCrowd.CompareTool/RightCrowd.CompareTool/ViewModels/ExportViewModel.cs
@@ -1,16 +1,118 @@
 ﻿using RightCrowd.CompareTool.HelperClasses;
+using RightCrowd.CompareTool.HelperClasses.EventHandlers.Export;
+using RightCrowd.CompareTool.HelperClasses.ExportHelpers.FileConverter;
+using RightCrowd.CompareTool.HelperClasses.ExportHelpers.Filter;
 using RightCrowd.CompareTool.HelperClasses.Providers.DisplayData;
+using RightCrowd.CompareTool.Models.Export.Configurations;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace RightCrowd.CompareTool
 {
     public class ExportViewModel : ObservableObject, IPageViewModel
     {
+        #region Fields
+        // Export classes
         private IDisplayDataProvider _displayDataProvider;
+        private IExportFilter _exportFilter;
+        private IFileConverter _txtFileConverter;
+        // Flags
+        private bool _includeSimilarFields;
+        private bool _db1Only;
+        private bool _db2Only;
+        // Commands
+        private ICommand _exportToText;
+        #endregion // Fields
+
+        #region Constructors
 
         public ExportViewModel(IDisplayDataProvider displayDataProvider)
         {
             _displayDataProvider = displayDataProvider;
+            _exportFilter = new ExportFilter();
+            _txtFileConverter = new TextFileConverter();
         }
+
+        #endregion // Constructors
+
+        #region ICommand Methods
+
+        public ICommand ExportToText
+        {
+            get
+            {
+                if (_exportToText == null)
+                    _exportToText = new RelayCommand(i => ExportToFile("txt"));
+                return _exportToText;
+            }
+        }
+
+        #endregion // ICommand Methods
+
+        #region Export Methods
+
+        private void ExportToFile(string format)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.Filter = $"(*.{format})|*.{format}";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                new ExportEventHandler(_exportFilter, _txtFileConverter, new ExportConfiguration(IncludeSimilarFields, DB1Only, DB2Only), _displayDataProvider).Export(dialog.FileName);
+            }
+        }
+
+        #endregion // Export Methods
+
+        #region Properties
+
+        public bool IncludeSimilarFields
+        {
+            get { return _includeSimilarFields; }
+            set
+            {
+                if (_includeSimilarFields != value)
+                {
+                    _includeSimilarFields = value;
+                    OnPropertyChanged("IncludeSimilarFields");
+                }
+            }
+        }
+
+        public bool DB1Only
+        {
+            get { return _db1Only; }
+            set
+            {
+                if(_db1Only != value)
+                {
+                    _db1Only = value;
+                    OnPropertyChanged("DB1Only");
+                    if (_db1Only)
+                        DB2Only = false;
+                }
+            }
+        }
+
+        public bool DB2Only
+        {
+            get { return _db2Only; }
+            set
+            {
+                if(_db2Only != value)
+                {
+                    _db2Only = value;
+                    OnPropertyChanged("DB2Only");
+                    if (_db2Only)
+                        DB1Only = false;
+                }
+            }
+        }
+
+        #endregion // Properties
 
         #region IPageViewModel Members
 
